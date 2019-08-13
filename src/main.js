2320df22bb1e04c01226f7ca68b325bd911f8761
Anna Kraeva <kraevaas@gmail.com>
2019-08-13 20:55:41 +0300

    add content sort and search to components and render them

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import {renderSearch} from './components/search.js';
 import {renderPrifile} from './components/profile.js';
+import {renderMenu} from './components/menu.js';
+import {renderSort} from './components/sort.js';
+import {renderMenu} from './components/menu.js';
+import {renderContent} from './components/content.js';
 
 const headContainer = document.querySelector('.header');
 headContainer.insertAdjacentHTML('beforeend', renderSearch());
 headContainer.insertAdjacentHTML('beforeend', renderPrifile());
+
+const mainContainer = document.querySelector('.main');
+mainContainer.insertAdjacentHTML('beforeend', renderMenu());
+mainContainer.insertAdjacentHTML('beforeend', renderSort());
+mainContainer.insertAdjacentHTML('beforeend', renderContent());
